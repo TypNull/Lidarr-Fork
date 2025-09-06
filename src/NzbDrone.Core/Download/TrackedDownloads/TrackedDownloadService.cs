Sonarr/Sonarr

New: Option to treat downloads with non-media extensions as failed
Closes #7369

src/NzbDrone.Core/Download/TrackedDownloads/TrackedDownloadService.cs
@@ -166,6 +166,11 @@ namespace NzbDrone.Core.Download.TrackedDownloads
                         {
                             trackedDownload.RemoteEpisode.Release.IndexerFlags = flags;
                         }
+
+                        if (downloadHistory != null)
+                        {
+                            trackedDownload.RemoteEpisode.Release.IndexerId = downloadHistory.IndexerId;
+                        }
                     }
                 }
 

TypNull/Lidarr-Fork

(cherry picked from commit 776143cc813ec1b5fa31fbf8667c3ab174b71f5c)

src/NzbDrone.Core/Download/TrackedDownloads/TrackedDownloadService.cs
@@ -193,7 +193,14 @@ namespace NzbDrone.Core.Download.TrackedDownloads
                         Enum.TryParse(grabbedEvent?.Data?.GetValueOrDefault("indexerFlags"), true, out IndexerFlags flags))
                     {
                         trackedDownload.RemoteAlbum.Release ??= new ReleaseInfo();
+                        trackedDownload.RemoteAlbum.Release.Indexer = trackedDownload.Indexer;
+                        trackedDownload.RemoteAlbum.Release.Title = trackedDownload.RemoteAlbum.ParsedAlbumInfo?.ReleaseTitle;
                         trackedDownload.RemoteAlbum.Release.IndexerFlags = flags;
+
+                        if (downloadHistory != null)
+                        {
+                            trackedDownload.RemoteAlbum.Release.IndexerId = downloadHistory.IndexerId;
+                        }
                     }
                 }
 